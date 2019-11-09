e2470e51f9927b490a28fa78b7388e0212fb82f2
whitejamer <muxah3258@gmail.com>
2019-11-09 16:06:46 +0300

    Create Post Model, make migrations and add app in 'INSTALLED_APPS'

--- postmanager/models.py
+++ postmanager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from uprofile.models import User
+from django.urls import reverse_lazy
+from django.urls import reverse
+from pytils.translit import slugify # для перевода киррилицы в латиницу
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    slug = models.SlugField(blank=True)
+    owner = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, blank=True, null=True)
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, *kwargs)
+    def get_absolute_url(self):
+        return reverse('postmanager:post_detail', kwargs={'slug': self.slug})
