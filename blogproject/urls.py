591cea24738820feb28c37b79365691c6ddfa2f2
whitejamer <muxah3258@gmail.com>
2019-11-11 20:47:25 +0300

    Configure Auth system

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^u/', include('uprofile.urls')),
     url(r'^posts/', include('postmanager.urls')),
 ]
-
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url(r'^auth/', include('django.contrib.auth.urls')),
+]
