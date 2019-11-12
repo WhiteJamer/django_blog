180522ee593315c95bcdd85ace6cd6f25af58c56
whitejamer <muxah3258@gmail.com>
2019-11-13 00:55:48 +0300

    task #4 Configure media & static paths

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import url
 from django.conf.urls import include
@@ -8,4 +10,4 @@ urlpatterns = [
     url(r'^posts/', include('postmanager.urls')),
     url(r'^auth/', include('customauth.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
