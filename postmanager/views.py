c963eff56f7d120d0b30cc6265f1175e4d3e9826
whitejamer <muxah3258@gmail.com>
2019-11-16 01:30:56 +0300

    Rewrite PostCreate-view, now need required field - 'owner'

--- postmanager/views.py
+++ postmanager/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, View
 from .models import Post
 from .forms import PostForm
-from .urls import *
 from django.urls import reverse_lazy
 
 
 class PostList(ListView):
     model = Post
+
 class PostDetail(DetailView):
     queryset = Post.objects.all()
-class PostCreate(FormView):
-    template_name = 'postmanager/post_add_form.html'
-    form_class = PostForm
-    success_url = reverse_lazy('postmanager:post_list')
-    def form_valid(self, form):
-        new_post = Post(title=form.cleaned_data['title'], content=form.cleaned_data['content'])
-        new_post.save()
-        return super(PostCreate, self).form_valid(form)
+
+class PostCreate(View):
+
+
+    def get(self, request):
+        context = {'form': PostForm}
+        return render(request, 'postmanager/post_add_form.html', context)
+
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid() and request.user.is_authenticated:
+            new_post = Post(
+                title=form.cleaned_data['title'],
+                content=form.cleaned_data['content'],
+                owner=request.user
+            )
+            new_post.save()
+            return redirect(new_post)
+        return redirect(reverse_lazy('postmanager:post_add'))
