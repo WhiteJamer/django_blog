ca30059fce556373f8eb27b24cb1572435cc0bdd
whitejamer <muxah3258@gmail.com>
2019-11-15 23:39:16 +0300

    override save() for generate slug

--- categorymanager/models.py
+++ categorymanager/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from pytils.translit import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(blank=True)
+
+    # Создаем слаг из поля name и делаем транслит
+    # если поле содержит киррилицу
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
