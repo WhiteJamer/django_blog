9b18159be4591e15630ea4350c8d78805a8dcadb
whitejamer <muxah3258@gmail.com>
2019-11-16 22:00:34 +0300

    Create CommentForm

---
+++ commentmanager/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, Textarea
+from .models import Comment
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        widgets = {
+            'body':Textarea(
+                attrs={'class':'form-control'}
+            )
+        }
