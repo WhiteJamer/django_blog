c6b6f35b1119757e3192f963e9dff6f88a980eda
whitejamer <muxah3258@gmail.com>
2019-11-16 20:22:16 +0300

    Create model Comment & make migrations

--- commentmanager/models.py
+++ commentmanager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from postmanager.models import Post
+from uprofile.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    body = models.TextField(max_length=700)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
