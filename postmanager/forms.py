beddd527082c3abc07f9d24f0d258fbb5901657c
whitejamer <muxah3258@gmail.com>
2019-11-17 20:09:26 +0300

    Add field categories in PostForm

--- postmanager/forms.py
+++ postmanager/forms.py
@@ -1,15 +1,18 @@
 from .models import Post
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, SelectMultiple
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'categories']
         widgets = {
             'title':TextInput(attrs={
                 'class':'form-control'
             }),
             'content': Textarea(attrs={
                 'class': 'form-control'
-            })
+            }),
+            'categories':SelectMultiple(attrs={
+                'class':'form-control'}
+            )
         }
