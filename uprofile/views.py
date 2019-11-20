df1b29b8df6e2769d802a33479c1edbe6e0cd765
whitejamer <muxah3258@gmail.com>
2019-11-21 01:24:27 +0300

    Did showing profile info & design user_detail.html

--- uprofile/views.py
+++ uprofile/views.py
@@ -4,6 +4,7 @@ from .models import User
 class UserList(ListView):
     model = User
 class UserDetail(DetailView):
+    context_object_name = 'auser' # another user not request.user
     queryset = User.objects.all()
 
 
