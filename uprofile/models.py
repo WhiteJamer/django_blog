8fee4d1f1467da10178ce7164c3cf5dd85104323
whitejamer <muxah3258@gmail.com>
2019-11-22 21:22:50 +0300

    Do slug unique=True, add avatar-field, update get_absolute_url & make migrations

--- uprofile/models.py
+++ uprofile/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.urls import reverse_lazy
 from django.contrib.auth.models import AbstractUser
+from time import time
+from pytils.translit import slugify
 
 class User(AbstractUser):
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
     rating = models.IntegerField(default=0, blank=True, null=True)
-    def save(self, *args, **kwargs): #Слаг по умолчанию
+    avatar = models.FileField(upload_to='avatars/', default='no-image-user.png')
+    def save(self, *args, **kwargs):
         if not self.id:
             self.slug = self.username
         super(User, self).save(*args, **kwargs)
     def get_absolute_url(self):
-        return self.slug
+        return reverse_lazy('uprofile:user_detail', kwargs={'slug': self.slug})
 
