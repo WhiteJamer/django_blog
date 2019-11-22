74cdd3534800ef240c11b770a2783b6afdb04527
WhiteJamer <33582148+WhiteJamer@users.noreply.github.com>
2019-11-22 21:42:33 +0300

    Made it avatar-upload & design display (#34)
    * Add ProfileForm with field 'avatar'
    
    * Do slug unique=True, add avatar-field, update get_absolute_url & make migrations
    
    * Add url 'profile_update'
    
    * Made it possible image-upload
    
    * Design elipse avatars in templates

--- uprofile/views.py
+++ uprofile/views.py
@@ -1,10 +1,37 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.views.generic import ListView, DetailView, View, UpdateView
 from .models import User
+from .forms import ProfileForm
+from django.core.files.storage import FileSystemStorage
 
 class UserList(ListView):
     model = User
+
+
 class UserDetail(DetailView):
     context_object_name = 'auser' # another user not request.user
     queryset = User.objects.all()
+
+
+class ProfileUpdate(View):
+
+    def get(self, request, slug):
+        user = get_object_or_404(User, slug__iexact=slug)
+        form = ProfileForm(instance=user)
+        context = {'form': form}
+        return render(request, 'uprofile/profile_update.html', context)
+
+    def post(self, request, slug):
+        user = get_object_or_404(User, slug__iexact=slug)
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data['avatar']
+            fs = FileSystemStorage()
+            avatar = fs.save(image.name, image)
+            user.avatar = avatar
+            user.save()
+            return redirect(user)
+        return redirect(reverse('uprofile:profile_update', kwargs={'slug':slug}))
+
 
 
