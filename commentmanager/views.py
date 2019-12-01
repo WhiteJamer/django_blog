a51b4062025c455cb8bbeaad284ff0428e4f2f6a
whitejamer <muxah3258@gmail.com>
2019-12-02 02:00:30 +0300

    Made it ajax-interaction for comments in post-detail.html

--- commentmanager/views.py
+++ commentmanager/views.py
@@ -6,11 +6,12 @@ from django.views.generic import View
 from .models import Comment
 from .forms import CommentForm
 from el_pagination.views import AjaxListView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 class CommentList(AjaxListView):
     model = Comment
     context_object_name = 'comments'
+
     def get_queryset(self):
         if self.request.GET.get('owner'):
             owner = self.request.GET.get('owner')
@@ -19,6 +20,14 @@ class CommentList(AjaxListView):
         else:
             queryset = Comment.objects.all()
             return queryset
+
+
+class PostCommentList(View):
+
+    def get(self, request, postslug):
+        comments = Comment.objects.filter(post__slug__iexact=postslug)
+        context = {'comments': comments}
+        return render(request, 'commentmanager/includes/comment_list_page.html', context)
 
 
 class CommentCreate(View):
@@ -35,25 +44,57 @@ class CommentCreate(View):
             )
             new_comment.save()
             redirect(post)
-        return redirect(reverse('postmanager:post_detail', kwargs={'slug':post.slug}) + '?message=Not valid data')
+        return redirect(reverse('postmanager:post_detail', kwargs={'slug':post.slug}))
+
+
+class CommentUpdate(View):
+
+    @method_decorator(login_required)
+    def get(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if request.user == comment.owner:
+            form = CommentForm(instance=comment)
+            context = {'comment': comment, 'form': form}
+            return render(request, 'commentmanager/includes/comment_update_modal.html', context)
+        else:
+            data = {'data': 'You is not owner'}
+            return JsonResponse(data)
+
+    @method_decorator(login_required)
+    def post(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if request.user == comment.owner:
+            form = CommentForm(request.POST, instance=comment)
+            form.save()
+            data = {'data': 'Comment has been updated'}
+            return JsonResponse(data)
+        else:
+            data = {'data': 'You is not owner'}
+            return JsonResponse(data)
 
 
 class CommentDelete(View):
 
     @method_decorator(login_required)
+    def get(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if request.user == comment.owner:
+            context = {'comment': comment}
+            return render(request, 'commentmanager/includes/comment_delete_modal.html', context)
+        else:
+            data = {'data': 'You is not owner'}
+            return JsonResponse(data)
+
+    @method_decorator(login_required)
     def post(self, request, pk):
         comment = Comment.objects.get(pk=pk)
-        if request.is_ajax():
-            if request.user == comment.owner:
-                comment.delete()
-                data = {'info': 'Comment has been deleted'}
-            else:
-                data = {'info': 'You is not owner'}
-            return JsonResponse(data)
+        if request.user == comment.owner:
+            comment.delete()
+            data = {'info': 'Comment has been deleted'}
         else:
-            if request.user == comment.owner:
-                comment.delete()
-                return redirect(request.META.get('HTTP_REFERER'))
+            data = {'info': 'You is not owner'}
+        return JsonResponse(data)
+
 
 
 
