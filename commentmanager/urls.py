85ab952383c8a95b064844b1b829013b9d854ffc
WhiteJamer <33582148+WhiteJamer@users.noreply.github.com>
2019-12-02 02:08:30 +0300

    Ajax interaction for comments (#53)
    * Upgrade ajax interaction with categories
    
    * Made it ajax-interaction for comments in post-detail.html

--- commentmanager/urls.py
+++ commentmanager/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import CommentList, CommentCreate, CommentDelete
+from .views import CommentList, CommentCreate, CommentDelete, CommentUpdate, PostCommentList
 
 app_name = 'commentmanager'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^$', CommentList.as_view(), name='comment_list'),
     url(r'^add_comment/(?P<post_slug>[\w-]+)/$', CommentCreate.as_view(), name='comment_add'),
 
-    url(r'^(?P<pk>\d+)/delete$', CommentDelete.as_view(), name='comment_delete'), # fixit
+    url(r'^(?P<pk>\d+)/delete/$', CommentDelete.as_view(), name='comment_delete'),
+    url(r'^(?P<pk>\d+)/update/$', CommentUpdate.as_view(), name='comment_update'),
+    url(r'^(?P<postslug>[\w-]+)/$', PostCommentList.as_view(), name='comment_list'),
 ]
