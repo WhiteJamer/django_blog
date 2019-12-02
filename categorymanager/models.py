9d9338a770742dccca505e884a038f4984f23277
whitejamer <muxah3258@gmail.com>
2019-12-02 18:18:07 +0300

    Category-model: Add for name & slug unique=True

--- categorymanager/models.py
+++ categorymanager/models.py
@@ -3,8 +3,8 @@ from django.urls import reverse_lazy
 from pytils.translit import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(blank=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     def __str__(self):
         return self.name
     # Создаем слаг из поля name и делаем транслит
