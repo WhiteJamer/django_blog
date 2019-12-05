5b110776b0b8c4805d6769246816e042e20b67d8
whitejamer <muxah3258@gmail.com>
2019-12-05 18:21:33 +0300

    Change settings.py add STATIC_ROOT for production

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'uprofile.User'
