13ac2e0c180b1d581aeeaca703812d35f0e6017d
WhiteJamer <33582148+WhiteJamer@users.noreply.github.com>
2020-01-14 19:56:22 +0300

    Fixes for easy deployment (#66)
    * Display TinyMCE in Admin-display
    
    * Fix slug generate
    
    * Comments load small fix
    
    * Make production settings

--- postmanager/models.py
+++ postmanager/models.py
@@ -3,8 +3,10 @@ from uprofile.models import User
 from categorymanager.models import Category
 from django.urls import reverse_lazy
 from django.urls import reverse
-from pytils.translit import slugify # для перевода киррилицы в латиницу
+from pytils.translit import slugify  # для перевода киррилицы в латиницу
 from tinymce.models import HTMLField
+from time import time
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -13,9 +15,16 @@ class Post(models.Model):
     owner = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField(Category, related_name='posts', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.slug = slugify(self.title)
+        if not self.id and not self.slug:
+            self.slug = str(int(time())) + '_' + slugify(self.title)
+        elif not self.id and self.slug:
+            self.slug = slugify(self.slug)
         super(Post, self).save(*args, *kwargs)
+
     def get_absolute_url(self):
         return reverse('postmanager:post_detail', kwargs={'slug': self.slug})
